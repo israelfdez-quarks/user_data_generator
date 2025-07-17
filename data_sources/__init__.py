833c38d0362f47d5721d88ebc825344c25e0810f
Israel Fdez <israel@quarksalchemist.com>
2025-07-17 14:15:22 -0300

    Clean up __init__.py by removing redundant comments and unused docstring
    Signed-off-by: Israel Fdez <israel@quarksalchemist.com>

--- data_sources/__init__.py
+++ data_sources/__init__.py
@@ -1,19 +1,10 @@
-"""
-Data sources module for retrieving user data from various sources.
 
-This module provides a unified interface for accessing user data from different sources,
-such as Higyrus API, BeClever database, AL2Sync database, and mock data for testing.
-"""
-
-# Export the abstract base class
 from data_sources.abstract import DataSource
 from data_sources.abstract import combine_data_sources, save_to_csv
 from data_sources.al2sync import AL2SyncDataSource
 from data_sources.beclever import BeCleverDataSource
-# Export the concrete implementations
 from data_sources.higyrus import HigyrusDataSource
 
-# Define what's available when using "from data_sources import *"
 __all__ = [
     'DataSource',
     'HigyrusDataSource',
